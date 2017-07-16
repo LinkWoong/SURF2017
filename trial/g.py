20cb63a217045ff4116f6ccf0f8caea5028768a7
LinkWong <zuiaineo@foxmail.com>
2017-07-16 11:41:50 +0800

    untrained model, so crap

--- trial/g.py
+++ trial/g.py
@@ -1,16 +1,16 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-import numpy as np
 from keras_vgg19 import VGG19
-from keras.models import *
-from keras.layers import *
-from keras.optimizers import *
+import os   
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import matplotlib.pyplot as plt
+import pylab
 from keras import losses
 from keras.backend import tf as ktf
-from keras import backend as K
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.layers import *
 from keras.layers.merge import add
-import matplotlib.pyplot as plt
+from keras.models import *
+from keras.preprocessing.image import img_to_array, load_img
+
+from keras_vgg19 import VGG19
 
 
 def main_model():
@@ -30,7 +30,6 @@ def main_model():
     guide4 = Conv2DTranspose(filters=64, kernel_size=2, strides=2, padding="SAME", activation="relu")(guide3)
     guide5 = Conv2DTranspose(filters=32, kernel_size=2, strides=2, padding="SAME", activation="relu")(guide4)
     front_decoder_output = Dense(1, name="front_decoder_output")(guide5)
-    # plt.imshow(front_decoder_output)
     # The vgg model
     vgg_style = Lambda(lambda image: ktf.image.resize_images(image, (224, 224)))(style)
     vgg_layer = VGG19(input_tensor=vgg_style, input_shape=(224, 224, 3)).output
@@ -44,7 +43,6 @@ def main_model():
     guide9 = Conv2DTranspose(filters=64, kernel_size=2, strides=2, padding="SAME", activation="relu")(guide8)
     guide10 = Conv2DTranspose(filters=32, kernel_size=2, strides=2, padding="SAME", activation="relu")(guide9)
     end_decoder_output = Dense(3, name="end_decoder_output")(guide10)
-    # plt.imshow(end_decoder_output)
     # The end part of the network
     deconv7 = concatenate([deconv7, conv5], axis=3)
     deconv8 = Conv2DTranspose(filters=256, kernel_size=2, strides=2, padding='SAME', activation='relu')(deconv7)
@@ -56,17 +54,18 @@ def main_model():
     deconv11 = Conv2DTranspose(filters=32, kernel_size=2, strides=2, padding='SAME', activation='relu')(deconv10)
     deconv11 = concatenate([deconv11, conv1], axis=3)
     network_output = Dense(3, name="network_output")(deconv11)
-    # plt.imshow(network_output)
-    # The two form of style picture
+    # The two form of style picture (used to calculate the loss)
     style_256 = Lambda(lambda image: ktf.image.resize_images(image, (256, 256)))(style)
     gray_style = ktf.image.rgb_to_grayscale(style)
     # The model part
     model = Model(inputs=[sketch, style], outputs=[front_decoder_output, end_decoder_output, network_output])
+    # the ratio [1, 0.3, 0.9] is suggested in the paper
     model.compile(optimizer='sgd', loss={
         'front_decoder_output': losses.mean_absolute_error,
         'end_decoder_output': losses.mean_absolute_error,
         'network_output': losses.mean_absolute_error
     }, loss_weights=[1, 0.3, 0.9])
+    # The true loss --- waited to be implemented
     # 'dense_1': losses.mean_absolute_error(gray_style, front_decoder_output),
     # 'dense_3': losses.mean_absolute_error(style, end_decoder_output),
     # 'dense_4': losses.mean_absolute_error(style_256, network_output)
@@ -74,15 +73,21 @@ def main_model():
 
 if __name__ == '__main__':
     sketch_image = img_to_array(load_img('./Sketch.jpg', grayscale=True))
-    sketch_image = K.expand_dims(sketch_image, axis=0)
-    print(sketch_image.shape)
+    sketch_image = np.expand_dims(sketch_image, axis=0)
     style_image = img_to_array(load_img('./Style.jpg'))
-    style_image = K.expand_dims(style_image, axis=0)
-    print(style_image.shape)
+    style_image = np.expand_dims(style_image, axis=0)
     network = main_model()
-    print(network)
-    generate_image = network.predict({'sketch_input': sketch_image, 'style_input': style_image}, batch_size=1)
-    # output = np.random.random((1, 1, 1, 1))
-    # output = tuple(output)
-    # print(output)
-# network.fit([sketch_image, style_image], list(np.zeros((1, 256, 256, 3))), batch_size=1)
+    generate_image_list = network.predict({'sketch_input': sketch_image, 'style_input': style_image})
+    [front_image, end_image, network_image] = generate_image_list
+
+    front_image = front_image.reshape([512, 512])
+    plt.imshow(front_image, cmap='gray')
+    pylab.show()
+
+    network_image = network_image.reshape([256, 256, 3])
+    plt.imshow(network_image)
+    pylab.show()
+
+    end_image = end_image.reshape([512, 512, 3])
+    plt.imshow(end_image)
+pylab.show()
