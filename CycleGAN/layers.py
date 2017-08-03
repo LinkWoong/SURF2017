4e0196d6c762f594d0dff527d79d3ddc5e11f9f4
LinkWong <zuiaineo@foxmail.com>
2017-08-03 17:05:20 +0800

    move

--- CycleGAN/layers.py
+++ CycleGAN/layers.py
@@ -11,6 +11,9 @@ img_size = img_height * img_width
 batch_size = 1
 pool_size = 50
 
+ngf = 32
+ndf = 64
+
 
 def instance_norm(x):
 
@@ -32,8 +35,8 @@ def leakyrelu(x, leaky=0.2, name='leakyrelu'):
 
     with tf.variable_scope(name):
 
-        f1 = 0.5 * (1 + leak)
-        f2 = 0.5 * (1 - leak)
+        f1 = 0.5 * (1 + leaky)
+        f2 = 0.5 * (1 - leaky)
 
     return f1 * x + f2 * abs(x)
 
@@ -62,7 +65,7 @@ def deconv2d(inputconv, output, o_d=64, f_h=7, f_w=7, s_h=1, s_w=1, stddev=0.02,
 
     with tf.variable_scope(name):
 
-        conv = tf.contrib.layers.conv2d_transpose(inputconv, o_d, [f_h, f_w], [s_h, s_w],padding, activation_fn=None, weights_initializer=tf.truncated_normal_initializer(stddev=stddev), bias_initializer=tf.constant_initializer(0.0))
+        conv = tf.contrib.layers.conv2d_transpose(inputconv, o_d, [f_h, f_w], [s_h, s_w],padding, activation_fn=None, weights_initializer=tf.truncated_normal_initializer(stddev=stddev), biases_initializer=tf.constant_initializer(0.0))
 
         if do_norm:
 
@@ -80,17 +83,16 @@ def deconv2d(inputconv, output, o_d=64, f_h=7, f_w=7, s_h=1, s_w=1, stddev=0.02,
 
         return conv
 
-def build_resnet_block(inputconv, dim, name='resnet'):
-
+def build_resnet_block(inputres, dim, name="resnet"):
+    
     with tf.variable_scope(name):
 
-        out_res = tf.pad(inputconv, [[0, 0], [1, 1], [1, 1], [0, 0]], 'REFLECT')
-        out_res = conv2d(out_res, dim, 3, 3, 1, 1, 0.02, 'VALID', 'c1')
-        out_res = tf.pad(out_res, [[0, 0], [1, 1], [1, 1], [0, 0]], 'REFLECT')
-        out_res = conv2d(out_res, dim, 3, 3, 1, 1,0.02, 'VALID', 'c2', do_relu=False)
-
-    return tf.nn.relu(out_res + inputconv)
-
+        out_res = tf.pad(inputres, [[0, 0], [1, 1], [1, 1], [0, 0]], "REFLECT")
+        out_res = conv2d(out_res, dim, 3, 3, 1, 1, 0.02, "VALID","c1")
+        out_res = tf.pad(out_res, [[0, 0], [1, 1], [1, 1], [0, 0]], "REFLECT")
+        out_res = conv2d(out_res, dim, 3, 3, 1, 1, 0.02, "VALID","c2",do_relu=False)
+        
+        return tf.nn.relu(out_res + inputres)
 
 
 def build_generator_resnet_6blocks(inputconv, name='generator'):
@@ -120,33 +122,36 @@ def build_generator_resnet_6blocks(inputconv, name='generator'):
     return output
 
 
-def build_generator_resnet_9blocks(inputconv, name='generator'):
-
+def build_generator_resnet_9blocks(inputgen, name="generator"):
     with tf.variable_scope(name):
+        f = 7
+        ks = 3
+        
+        pad_input = tf.pad(inputgen,[[0, 0], [ks, ks], [ks, ks], [0, 0]], "REFLECT")
+        o_c1 = conv2d(pad_input, ngf, f, f, 1, 1, 0.02,name="c1")
+        o_c2 = conv2d(o_c1, ngf*2, ks, ks, 2, 2, 0.02,"SAME","c2")
+        o_c3 = conv2d(o_c2, ngf*4, ks, ks, 2, 2, 0.02,"SAME","c3")
 
-        pad_input = tf.pad(inputconv, [[0, 0], [3, 3], [3, 3], [0, 0]], 'REFLECT')
-        o_c1 = conv2d(pad_input, 32, 7, 7, 1, 1, 0.02,name='c1')
-        o_c2 = conv2d(o_c1, 32*2, 7, 7, 1, 1, 0.02,'SAME','c2')
-        o_c3 = conv2d(o_c2, 32*4, 7, 7, 1, 1, 0.02,'SAME','c3')
+        o_r1 = build_resnet_block(o_c3, ngf*4, "r1")
+        o_r2 = build_resnet_block(o_r1, ngf*4, "r2")
+        o_r3 = build_resnet_block(o_r2, ngf*4, "r3")
+        o_r4 = build_resnet_block(o_r3, ngf*4, "r4")
+        o_r5 = build_resnet_block(o_r4, ngf*4, "r5")
+        o_r6 = build_resnet_block(o_r5, ngf*4, "r6")
+        o_r7 = build_resnet_block(o_r6, ngf*4, "r7")
+        o_r8 = build_resnet_block(o_r7, ngf*4, "r8")
+        o_r9 = build_resnet_block(o_r8, ngf*4, "r9")
 
-        o_r1 = build_resnet_block(o_c3, 32*4, 'r1')
-        o_r2 = build_resnet_block(o_r1, 32*4, 'r2')
-        o_r3 = build_resnet_block(o_r2, 32*4, 'r3')
-        o_r4 = build_resnet_block(o_r3, 32*4, 'r4')
-        o_r5 = build_resnet_block(o_r4, 32*4, 'r5')
-        o_r6 = build_resnet_block(o_r5, 32*4, 'r6')
-        o_r7 = build_resnet_block(o_r6, 32*4, 'r7')
-        o_r8 = build_resnet_block(o_r7, 32*4, 'r8')
-        o_r9 = build_resnet_block(o_r8, 32*4, 'r9')
+        o_c4 = deconv2d(o_r9, [batch_size,128,128,ngf*2], ngf*2, ks, ks, 2, 2, 0.02,"SAME","c4")
+        o_c5 = deconv2d(o_c4, [batch_size,256,256,ngf], ngf, ks, ks, 2, 2, 0.02,"SAME","c5")
+        o_c6 = conv2d(o_c5, img_layer, f, f, 1, 1, 0.02,"SAME","c6",do_relu=False)
 
-        o_c4 = deconv2d(o_r9, [batch_size, 128, 128, 32*2], 32*2, 7, 7, 2, 2, 0.02, 'SAME', 'c4')
-        o_c5 = deconv2d(o_c4, [batch_size, 256, 256, 32], 32, 7, 7, 2, 2, 0.02, 'SAME', 'c5')
-        o_c6 = deconv2d(o_c5, img_layer, 7, 7, 1, 1, 0.02, 'SAME', 'c6', do_relu=False)
+        # Adding the tanh layer
+
+        out_gen = tf.nn.tanh(o_c6,"t1")
 
 
-        output = tf.nn.tanh(o_c6, 't1')
-
-    return output
+        return out_gen
 
 def build_discriminator(inputconv, name='discriminator'):
 
