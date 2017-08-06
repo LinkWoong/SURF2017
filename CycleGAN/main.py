cc8ffd8b9c5ba1aabe4710924ebcf5cddb0d4ded
LinkWong <zuiaineo@foxmail.com>
2017-08-06 16:15:40 +0800

    added GPU support

--- CycleGAN/main.py
+++ CycleGAN/main.py
@@ -1,4 +1,4 @@
-import os   
+import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 import numpy as np
@@ -17,6 +17,7 @@ img_width = 256
 img_layer = 3
 img_size = img_height * img_width
 
+log_device_placement = True
 to_train = True
 to_test = False
 to_restore = False
@@ -43,15 +44,15 @@ class CycleGAN():
 
     def input_setup(self):
 
-        
-        filenames_A = tf.train.match_filenames_once("./input/trainA/*.jpg")    
+
+        filenames_A = tf.train.match_filenames_once("./input/trainA/*.jpg")
         self.queue_length_A = tf.size(filenames_A)
-        filenames_B = tf.train.match_filenames_once("./input/trainB/*.jpg")    
+        filenames_B = tf.train.match_filenames_once("./input/trainB/*.jpg")
         self.queue_length_B = tf.size(filenames_B)
-        
+
         filename_queue_A = tf.train.string_input_producer(filenames_A)
         filename_queue_B = tf.train.string_input_producer(filenames_B)
-        
+
         image_reader = tf.WholeFileReader()
         _, image_file_A = image_reader.read(filename_queue_A)
         _, image_file_B = image_reader.read(filename_queue_B)
@@ -59,7 +60,7 @@ class CycleGAN():
         self.image_A = tf.subtract(tf.div(tf.image.resize_images(tf.image.decode_jpeg(image_file_A),[256,256]),127.5),1)
         self.image_B = tf.subtract(tf.div(tf.image.resize_images(tf.image.decode_jpeg(image_file_B),[256,256]),127.5),1)
 
-    
+
 
     def input_read(self, sess):
 
@@ -76,14 +77,14 @@ class CycleGAN():
         self.A_input = np.zeros((max_images, batch_size, img_height, img_width, img_layer))
         self.B_input = np.zeros((max_images, batch_size, img_height, img_width, img_layer))
 
-        for i in range(max_images): 
+        for i in range(max_images):
             image_tensor = sess.run(self.image_A)
-            if len(image_tensor) == img_size*batch_size*img_layer:
+            if (image_tensor.size == img_size*batch_size*img_layer):
                 self.A_input[i] = image_tensor.reshape((batch_size,img_height, img_width, img_layer))
 
         for i in range(max_images):
             image_tensor = sess.run(self.image_B)
-            if len(image_tensor) == img_size*batch_size*img_layer:
+            if (image_tensor.size == img_size*batch_size*img_layer):
                 self.B_input[i] = image_tensor.reshape((batch_size,img_height, img_width, img_layer))
 
 
@@ -97,7 +98,7 @@ class CycleGAN():
 
         self.input_A = tf.placeholder(tf.float32, [batch_size, img_width, img_height, img_layer], name="input_A")
         self.input_B = tf.placeholder(tf.float32, [batch_size, img_width, img_height, img_layer], name="input_B")
-        
+
         self.fake_pool_A = tf.placeholder(tf.float32, [None, img_width, img_height, img_layer], name="fake_pool_A")
         self.fake_pool_B = tf.placeholder(tf.float32, [None, img_width, img_height, img_layer], name="fake_pool_B")
 
@@ -108,7 +109,7 @@ class CycleGAN():
         self.lr = tf.placeholder(tf.float32, shape=[], name="lr")
 
         with tf.variable_scope("Model") as scope:
-            
+
             self.fake_B = build_generator_resnet_9blocks(self.input_A, name="g_A")
             self.fake_A = build_generator_resnet_9blocks(self.input_B, name="g_B")
             self.rec_A = build_discriminator(self.input_A, "d_A")
@@ -130,17 +131,17 @@ class CycleGAN():
 
 
         cyc_loss = tf.reduce_mean(tf.abs(self.input_A-self.cyc_A)) + tf.reduce_mean(tf.abs(self.input_B-self.cyc_B))
-        
+
         disc_loss_A = tf.reduce_mean(tf.squared_difference(self.fake_rec_A,1))
         disc_loss_B = tf.reduce_mean(tf.squared_difference(self.fake_rec_B,1))
-        
+
         g_loss_A = cyc_loss*10 + disc_loss_B
         g_loss_B = cyc_loss*10 + disc_loss_A
 
         d_loss_A = (tf.reduce_mean(tf.square(self.fake_pool_rec_A)) + tf.reduce_mean(tf.squared_difference(self.rec_A,1)))/2.0
         d_loss_B = (tf.reduce_mean(tf.square(self.fake_pool_rec_B)) + tf.reduce_mean(tf.squared_difference(self.rec_B,1)))/2.0
 
-        
+
         optimizer = tf.train.AdamOptimizer(self.lr, beta1=0.5)
 
         self.model_vars = tf.trainable_variables()
@@ -149,7 +150,7 @@ class CycleGAN():
         g_A_vars = [var for var in self.model_vars if 'g_A' in var.name]
         d_B_vars = [var for var in self.model_vars if 'd_B' in var.name]
         g_B_vars = [var for var in self.model_vars if 'g_B' in var.name]
-        
+
         self.d_A_trainer = optimizer.minimize(d_loss_A, var_list=d_A_vars)
         self.d_B_trainer = optimizer.minimize(d_loss_B, var_list=d_B_vars)
         self.g_A_trainer = optimizer.minimize(g_loss_A, var_list=g_A_vars)
@@ -196,15 +197,15 @@ class CycleGAN():
 
     def train(self):
 
-        self.input_setup()  
+        self.input_setup()
         self.model_setup()
 
         self.loss_calc()
-      
-        init = ([tf.global_variables_initializer(), tf.local_variables_initializer()])
-        saver = tf.train.Saver()     
 
-        with tf.Session() as sess:
+        init = ([tf.global_variables_initializer(), tf.local_variables_initializer()])
+        saver = tf.train.Saver()
+
+        with tf.Session(config=tf.ConfigProto(log_device_placement=log_device_placement)) as sess:
             sess.run(init)
 
             self.input_read(sess)
@@ -218,7 +219,7 @@ class CycleGAN():
             if not os.path.exists(check_dir):
                 os.makedirs(check_dir)
 
-            for epoch in range(sess.run(self.global_step),100):                
+            for epoch in range(sess.run(self.global_step),100):
                 print ("In the epoch ", epoch)
                 saver.save(sess,os.path.join(check_dir,"cyclegan"),global_step=epoch)
                 if(epoch < 100) :
@@ -236,31 +237,31 @@ class CycleGAN():
                     # Optimizing the G_A network
 
                     _, fake_B_temp, summary_str = sess.run([self.g_A_trainer, self.fake_B, self.g_A_loss_summ],feed_dict={self.input_A:self.A_input[ptr], self.input_B:self.B_input[ptr], self.lr:curr_lr})
-                    
-                    writer.add_summary(summary_str, epoch*max_images + ptr)                    
+
+                    writer.add_summary(summary_str, epoch*max_images + ptr)
                     fake_B_temp1 = self.fake_image_pool(self.num_fake_inputs, fake_B_temp, self.fake_images_B)
-                    
+
                     # Optimizing the D_B network
                     _, summary_str = sess.run([self.d_B_trainer, self.d_B_loss_summ],feed_dict={self.input_A:self.A_input[ptr], self.input_B:self.B_input[ptr], self.lr:curr_lr, self.fake_pool_B:fake_B_temp1})
                     writer.add_summary(summary_str, epoch*max_images + ptr)
-                    
-                    
+
+
                     # Optimizing the G_B network
                     _, fake_A_temp, summary_str = sess.run([self.g_B_trainer, self.fake_A, self.g_B_loss_summ],feed_dict={self.input_A:self.A_input[ptr], self.input_B:self.B_input[ptr], self.lr:curr_lr})
 
                     writer.add_summary(summary_str, epoch*max_images + ptr)
-                    
-                    
+
+
                     fake_A_temp1 = self.fake_image_pool(self.num_fake_inputs, fake_A_temp, self.fake_images_A)
 
                     # Optimizing the D_A network
                     _, summary_str = sess.run([self.d_A_trainer, self.d_A_loss_summ],feed_dict={self.input_A:self.A_input[ptr], self.input_B:self.B_input[ptr], self.lr:curr_lr, self.fake_pool_A:fake_A_temp1})
 
                     writer.add_summary(summary_str, epoch*max_images + ptr)
-                    
+
                     self.num_fake_inputs+=1
-            
-                        
+
+
 
                 sess.run(tf.assign(self.global_step, epoch + 1))
 
@@ -277,7 +278,7 @@ class CycleGAN():
         saver = tf.train.Saver()
         init = ([tf.global_variables_initializer(), tf.local_variables_initializer()])
 
-        with tf.Session() as sess:
+        with tf.Session(config=tf.ConfigProto(log_device_placement=log_device_placement)) as sess:
 
             sess.run(init)
 
@@ -287,7 +288,7 @@ class CycleGAN():
             saver.restore(sess, chkpt_fname)
 
             if not os.path.exists("./output/imgs/test/"):
-                os.makedirs("./output/imgs/test/")            
+                os.makedirs("./output/imgs/test/")
 
             for i in range(0,100):
                 fake_A_temp, fake_B_temp = sess.run([self.fake_A, self.fake_B],feed_dict={self.input_A:self.A_input[i], self.input_B:self.B_input[i]})
@@ -297,7 +298,7 @@ class CycleGAN():
                 imsave("./output/imgs/test/inputB_"+str(i)+".jpg",((self.B_input[i][0]+1)*127.5).astype(np.uint8))
 
 def main():
-    
+
     model = CycleGAN()
     if to_train:
         model.train()
