71f234380add4d106ce652d0b71af478ac5f1260
LinkWong <zuiaineo@foxmail.com>
2017-08-12 21:36:37 +0800

    test function

--- LinkGAN/test.py
+++ LinkGAN/test.py
@@ -6,14 +6,30 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import cv2
 import pandas as pd 
 
-
 from keras.models import load_model
 from preprocess import *
+from essential import *
+
 
 path = '/media/linkwong/D/1girl'
 mod_path = '/media/linkwong/D/mod.h5'
 
+img_width = 512
+img_height = 512
+img_depth = 3
+
+alpha = 0.3
+beta = 0.9
+batch_size = 1
+pooling = 10
+epochs = 100
+global_step = 0
+
 content_to_sketch = True
+log_device_placement = True
+output_partition_graphs = True
+start_train = True
+start_test = True
 
 
 content_path, sketch_path = preprocess(path, mod_path, content_to_sketch)
@@ -21,4 +37,13 @@ content_path, sketch_path = preprocess(path, mod_path, content_to_sketch)
 print content_path
 print sketch_path
 
+class link():
 
+	def setup(self, content_path, sketch_path, global_step):
+
+		self.global_step = global_step
+		filename_content = tf.train.string_input_producer(tf.train.match_filenames_once(content_path + '/*.jpeg'))
+		filename_sketch = tf.train.string_input_producer(tf.train.match_filenames_once(sketch_path + '/*.jpeg'))
+		
+
+		
