f6fc92b0453b77735d3c3f6f4e4881e131365999
LinkWong <zuiaineo@foxmail.com>
2017-08-13 16:19:11 +0800

    test

--- LinkGAN/test.py
+++ LinkGAN/test.py
@@ -44,8 +44,49 @@ class link():
 	def setup(self, content_path, sketch_path, style_path, global_step):
 
 		self.global_step = global_step
-		filename_content = tf.train.string_input_producer(tf.train.match_filenames_once(content_path + '/*.jpeg'))
-		filename_sketch = tf.train.string_input_producer(tf.train.match_filenames_once(sketch_path + '/*.jpeg'))
+
+		self.filename_content = tf.train.string_input_producer(tf.train.match_filenames_once(content_path + '/*.jpeg'))
+		self.filename_sketch = tf.train.string_input_producer(tf.train.match_filenames_once(sketch_path + '/*.jpeg'))
+		self.filename_style = tf.train.string_input_producer(tf.train.match_filenames_once(style_path + '/*.jpeg'))
 
 		reader = tf.WholeFileReader()
-		
+
+		_, content_image = reader.read(filename_content)
+		_, sketch_image = reader.read(filename_sketch)
+		_, style_image = reader.read(filename_style)
+
+		content_tensor = tf.image.decode_jpeg(content_image)
+		sketch_tensor = tf.image.decode_jpeg(sketch_image)
+		style_tensor = tf.image.decode_jpeg(style_image)
+
+
+		init = ([tf.global_variables_initializer(), tf.local_variables_initializer()])
+
+		with tf.Session() as sess:
+
+			sess.run(init)
+
+			coord = tf.train.Coordinator()
+			threads = tf.train.start_queue_runners(coord=coord)
+
+			num_content = sess.run(tf.size(self.filename_content))
+			num_sketch = sess.run(tf.size(self.filename_sketch))
+			num_style = sess.run(tf.size(self.filename_style))
+
+			print num_content, num_sketch, num_style
+
+			self.content = sess.run(content_tensor)
+			self.sketch = sess.run(sketch_tensor)
+			self.style = sess.run(style_tensor)
+
+			print self.content
+			print self.sketch
+			print self.style
+
+			print self.sketch.shape
+
+			coord.request_stop()
+			coord.join(threads)
+
+
+
